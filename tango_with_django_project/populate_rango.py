be8994ecc51b3e905183d88c885f08c631499d6e
David Maxwell <maxwelld90@gmail.com>
2019-07-23 19:19:39 +0100

    Updated population script -- tweaked one of the URLs

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -20,7 +20,7 @@ def populate():
     
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.0/intro/tutorial01/',
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
          'views': 3},
         {'title': 'Django Rocks',
          'url': 'http://www.djangorocks.com',
