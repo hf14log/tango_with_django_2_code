fb8931fc21a8010c59ecebc2d3a01cee15153d98
David Maxwell <maxwelld90@gmail.com>
2019-07-28 16:51:39 +0100

    Updated tests for Chapter 4

--- progress_tests/tests_chapter4.py
+++ progress_tests/tests_chapter4.py
@@ -24,7 +24,7 @@ from django.conf import settings
 FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD TEST FAILURE =({os.linesep}================{os.linesep}"
 FAILURE_FOOTER = f"{os.linesep}"
 
-class Chapter4ProjectStructureTests(TestCase):
+class Chapter4TemplatesStructureTests(TestCase):
     """
     Have you set templates, static files and media files up correctly, as per the book?
     """
@@ -56,7 +56,7 @@ class Chapter4ProjectStructureTests(TestCase):
         
         template_dir_value = os.path.normpath(settings.TEMPLATE_DIR)
         template_dir_computed = os.path.normpath(self.templates_dir)
-        self.assertTrue(template_dir_value, template_dir_computed, f"{FAILURE_HEADER}Your TEMPLATE_DIR setting does not point to the expected path. Check your configuration, and try again.{FAILURE_FOOTER}")
+        self.assertEqual(template_dir_value, template_dir_computed, f"{FAILURE_HEADER}Your TEMPLATE_DIR setting does not point to the expected path. Check your configuration, and try again.{FAILURE_FOOTER}")
     
     def test_template_lookup_path(self):
         """
@@ -128,11 +128,131 @@ class Chapter4StaticMediaTests(TestCase):
     A series of tests to check whether static files and media files have been setup and used correctly.
     Also tests for the two required files -- rango.jpg and cat.jpg.
     """
-    pass
-
+    def setUp(self):
+        self.project_base_dir = os.getcwd()
+        self.static_dir = os.path.join(self.project_base_dir, 'static')
+        self.media_dir = os.path.join(self.project_base_dir, 'media')
+    
+    def test_does_static_directory_exist(self):
+        """
+        Tests whether the static directory exists in the correct location -- and the images subdirectory.
+        Also checks for the presence of rango.jpg in the images subdirectory.
+        """
+        does_static_dir_exist = os.path.isdir(self.static_dir)
+        does_images_static_dir_exist = os.path.isdir(os.path.join(self.static_dir, 'images'))
+        does_rango_jpg_exist = os.path.isfile(os.path.join(self.static_dir, 'images', 'rango.jpg'))
+        
+        self.assertTrue(does_static_dir_exist, f"{FAILURE_HEADER}The static directory was not found in the expected location. Check the instructions in the book, and try again.{FAILURE_FOOTER}")
+        self.assertTrue(does_images_static_dir_exist, f"{FAILURE_HEADER}The images subdirectory was not found in your static directory.{FAILURE_FOOTER}")
+        self.assertTrue(does_rango_jpg_exist, f"{FAILURE_HEADER}We couldn't locate the rango.jpg image in the /static/images/ directory. If you think you've included the file, make sure to check the file extension. Sometimes, a JPG can have the extension .jpeg. Be careful! It must be .jpg for this test.{FAILURE_FOOTER}")
+    
+    def test_does_media_directory_exist(self):
+        """
+        Tests whether the media directory exists in the correct location.
+        Also checks for the presence of cat.jpg.
+        """
+        does_media_dir_exist = os.path.isdir(self.media_dir)
+        does_cat_jpg_exist = os.path.isfile(os.path.join(self.media_dir, 'cat.jpg'))
+        
+        self.assertTrue(does_media_dir_exist, f"{FAILURE_HEADER}We couldn't find the /media/ directory in the expected location. Make sure it is in your project directory (at the same level as the manage.py module).{FAILURE_FOOTER}")
+        self.assertTrue(does_cat_jpg_exist, f"{FAILURE_HEADER}We couldn't find the cat.jpg image in /media/. Check the file extension; this is a common pitfall. It should .jpg. Not .png, .gif, or .jpeg!{FAILURE_FOOTER}")
+    
+    def test_static_and_media_configuration(self):
+        """
+        Performs a number of tests on your Django project's settings in relation to static files and user upload-able files..
+        """
+        static_dir_exists = 'STATIC_DIR' in dir(settings)
+        self.assertTrue(static_dir_exists, f"{FAILURE_HEADER}Your settings.py module does not have the variable STATIC_DIR defined.{FAILURE_FOOTER}")
+        
+        expected_path = os.path.normpath(self.static_dir)
+        static_path = os.path.normpath(settings.STATIC_DIR)
+        self.assertEqual(expected_path, static_path, f"{FAILURE_HEADER}The value of STATIC_DIR does not equal the expected path. It should point to your project root, with 'static' appended to the end of that.{FAILURE_FOOTER}")
+        
+        staticfiles_dirs_exists = 'STATICFILES_DIRS' in dir(settings)
+        self.assertTrue(staticfiles_dirs_exists, f"{FAILURE_HEADER}The required setting STATICFILES_DIRS is not present in your project's settings.py module. Check your settings carefully. So many students have mistyped this one.{FAILURE_FOOTER}")
+        self.assertEqual([static_path], settings.STATICFILES_DIRS, f"{FAILURE_HEADER}Your STATICFILES_DIRS setting does not match what is expected. Check your implementation against the instructions provided.{FAILURE_FOOTER}")
+        
+        staticfiles_dirs_exists = 'STATIC_URL' in dir(settings)
+        self.assertTrue(staticfiles_dirs_exists, f"{FAILURE_HEADER}The STATIC_URL variable has not been defined in settings.py.{FAILURE_FOOTER}")
+        self.assertEqual('/static/', settings.STATIC_URL, f"{FAILURE_HEADER}STATIC_URL does not meet the expected value of /static/. Make sure you have a slash at the end!{FAILURE_FOOTER}")
+        
+        media_dir_exists = 'MEDIA_DIR' in dir(settings)
+        self.assertTrue(media_dir_exists, f"{FAILURE_HEADER}The MEDIA_DIR variable in settings.py has not been defined.{FAILURE_FOOTER}")
+        
+        expected_path = os.path.normpath(self.media_dir)
+        media_path = os.path.normpath(settings.MEDIA_DIR)
+        self.assertEqual(expected_path, media_path, f"{FAILURE_HEADER}The MEDIA_DIR setting does not point to the correct path. Remember, it should have an absolute reference to tango_with_django_project/media/.{FAILURE_FOOTER}")
+        
+        media_root_exists = 'MEDIA_ROOT' in dir(settings)
+        self.assertTrue(media_root_exists, f"{FAILURE_HEADER}The MEDIA_ROOT setting has not been defined.{FAILURE_FOOTER}")
+        
+        media_root_path = os.path.normpath(settings.MEDIA_ROOT)
+        self.assertEqual(media_path, media_root_path, f"{FAILURE_HEADER}The value of MEDIA_ROOT does not equal the value of MEDIA_DIR.{FAILURE_FOOTER}")
+        
+        media_url_exists = 'MEDIA_URL' in dir(settings)
+        self.assertTrue(media_url_exists, f"{FAILURE_HEADER}The setting MEDIA_URL has not been defined in settings.py.{FAILURE_FOOTER}")
+        
+        media_url_value = settings.MEDIA_URL
+        self.assertEqual('/media/', media_url_value, f"{FAILURE_HEADER}Your value of the MEDIA_URL setting does not equal /media/. Check your settings!{FAILURE_FOOTER}")
+    
+    def test_context_processor_addition(self):
+        """
+        Checks to see whether the media context_processor has been added to your project's settings module.
+        """
+        context_processors_list = settings.TEMPLATES[0]['OPTIONS']['context_processors']
+        self.assertTrue('django.template.context_processors.media' in context_processors_list, f"{FAILURE_HEADER}The 'django.template.context_processors.media' context processor was not included. Check your settings.py module.{FAILURE_FOOTER}")
+        
 
 class Chapter4ExerciseTests(TestCase):
     """
     A series of tests to ensure that the exercise listing at the end of Chapter 4 has been completed.
     """
-    pass
+    def setUp(self):
+        self.project_base_dir = os.getcwd()
+        self.template_dir = os.path.join(self.project_base_dir, 'templates', 'rango')
+        self.about_response = self.client.get(reverse('about'))
+    
+    def test_about_template_exists(self):
+        """
+        Tests the about template -- if it exists, and whether or not the about() view makes use of it.
+        """
+        template_exists = os.path.isfile(os.path.join(self.template_dir, 'about.html'))
+        self.assertTrue(template_exists, f"{FAILURE_HEADER}The about.html template was not found in the expected location.{FAILURE_FOOTER}")
+    
+    def test_about_uses_template(self):
+        """
+        Checks whether the index view uses a template -- and the correct one!
+        """
+        self.assertTemplateUsed(self.about_response, 'rango/about.html', f"{FAILURE_HEADER}The about() view does not use the about.html template.{FAILURE_FOOTER}")
+    
+    def test_about_starts_with_doctype(self):
+        """
+        Is the <!DOCTYPE html> declaration on the first line of the about.html template?
+        """
+        self.assertTrue(self.about_response.content.decode().startswith('<!DOCTYPE html>'), f"{FAILURE_HEADER}Your about.html template does not start with <!DOCTYPE html> -- this is requirement of the HTML specification.{FAILURE_FOOTER}")
+    
+    def test_about_contains_required_text(self):
+        """
+        Checks to see whether the required text is on the rendered about page.
+        """
+        required = [
+            "here is the about page.",
+            "This tutorial has been put together by "
+        ]
+        
+        for required_str in required:
+            self.assertTrue(required_str in self.about_response.content.decode(), f"{FAILURE_HEADER}The expected string '{required_str}' was not found in the rendered /rango/about/ response.{FAILURE_FOOTER}")
+    
+    def test_about_contains_rango(self):
+        """
+        Checks whether the rendered about view has the picture of Rango.
+        """
+        required_str = f"<img src=\"{settings.STATIC_URL}images/rango.jpg\" alt=\"Picture of Rango\" />"
+        self.assertTrue(required_str in self.about_response.content.decode(), f"{FAILURE_HEADER}The HTML markup to include the image of Rango in the about template was not found. It needs to match exactly what we are looking for. Check the book.{FAILURE_FOOTER}")
+        
+    def test_about_contains_cat(self):
+        """
+        Checks whether the rendered about view has the picture of a cat.
+        """
+        required_str = f"<img src=\"{settings.MEDIA_URL}cat.jpg\" alt=\"Picture of a Cat\" />"
+        self.assertTrue(required_str in self.about_response.content.decode(), f"{FAILURE_HEADER}The HTML markup to include the image of a cat in the about template was not found. It needs to match exactly what we are looking for. Check the book.{FAILURE_FOOTER}")
